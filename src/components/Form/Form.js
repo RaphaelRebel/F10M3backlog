8b0f8adef7867d1eff16e2056b359c01caad7494
Raphael Rebel <raphaelrebel@live.com>
2022-12-15 19:50:26 +0100

    Les 11 af

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,58 @@
-
-import Search from '../Search/Search';
-import SearchButton from "../SearchButton/SearchButton"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import {filterGamesByTitle, filterGamesByPlatform} from "../../helpers/filterGames";
 
 const Form = () => {
+    const [inputs, setInputs] = useState([
+        {
+            id: "TITLE",
+            value: "",
+            label: "TITLE",
+            filter: filterGamesByTitle
+        },
+        {
+            id: "PLATFORM",
+            value: "",
+            label: "PLATFORM"
+        }
+    ]);
+
+    const onInputChange = (event) => {
+       let copy = [...inputs]
+       copy.map(input => {
+        if(input.id === event.target.id){
+            input.value = event.target.value
+        }
+       });
+
+       setInputs(copy)
+    }
+    const inputsToBeRendered = inputs.map(objectFromStateArray => {
+       return <div>
+        <label htmlFor={objectFromStateArray.id}>{objectFromStateArray.label}</label>
+        <input onChange={onInputChange} id={objectFromStateArray.id} type="text" value={objectFromStateArray.value} />
+    </div>
+    })
+
+    
+
+    //Pas input state aan wanneer er een aanpassing is gemaakt in de input
+
+   
 
     const submit = (event) => {
         event.preventDefault()
+            console.log(filterGamesByPlatform("WII"))
     }
 
     return(
         <form onSubmit={submit}>
-            <Search />
-            <SearchButton />
+       { inputsToBeRendered}
+            <button onClick={submit}>Zoeken</button>
         </form>
     )
+
+    
 }
 
 export default Form
--- src/helpers/filterGames.js
+++ src/helpers/filterGames.js
@@ -1,9 +1,25 @@
 import {games} from "../data/games"
 
-export const filterGames = (nameToBeSearched) => {
+ export const filterGamesByTitle = (nameToBeSearched) => {
     return games.filter(game => {
+
         if(game.title.toUpperCase().indexOf(nameToBeSearched.toUpperCase()) !== -1 ){
             return game
         }
+        
+    })
+}
+
+export const filterGamesByPlatform = (platformToBeSearched) => {
+    return games.filter(game => {
+       let found = false;
+       game.platforms.forEach(platform => {
+        if(platform === platformToBeSearched){
+            found = true
+        }
+       })
+       if(found === true){
+        return game
+       }
     })
 }
